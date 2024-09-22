3150c865254ac4d9682544d1ace37c1dd699c5b9
douglasmg7 <douglasmg7@gmail.com>
2024-09-22 12:25:14 -0300

    Lesson 170 finished

--- models/event.go
+++ models/event.go
@@ -2,21 +2,39 @@ package models
 
 import (
 	"time"
+
+	"github.com/douglasmg7/gin_rest_api.git/db"
 )
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
-	Place       string    `binding:"required"`
+	Location    string    `binding:"required"`
 	DateTime    time.Time `binding:"required"`
 	UserID      int
 }
 
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	query := ` INSERT INTO events(name, description, location, dateTime, user_id)
+				VALUES(?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	_, err = result.LastInsertId()
+	// id, err := result.LastInsertId()
+	// e.ID = id
+
+	return err
 }
 
 func GetAllEvents() []Event {
